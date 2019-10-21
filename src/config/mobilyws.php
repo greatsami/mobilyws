f81330862ce884e35cb3f31eec0361f1a0b7cfe4
Sami Mansour <samiman.net@gmail.com>
2019-10-21 12:07:02 +0300

    Update Plugin

--- src/config/mobilyws.php
+++ src/config/mobilyws.php
@@ -4,8 +4,8 @@ return [
 
     'mobile'            => '',
     'password'          => '',
-    'apiKey'            => '9d76fee097b9aaccd8aca25807a13135',
-    'sender'            => 'Bluebees',
+    'apiKey'            => '',
+    'sender'            => '',
     'MsgID'             => rand(1,99999),
     'timeSend'          => 0,
     'dateSend'          => 0,
