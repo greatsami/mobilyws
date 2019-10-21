1af3c76cb18d08f38e4aae19a81c89083002b243
Sami Mansour <samiman.net@gmail.com>
2019-10-21 13:35:13 +0300

    update provider

--- src/MobilywsServiceProvider.php
+++ src/MobilywsServiceProvider.php
@@ -9,15 +9,16 @@ class MobilywsServiceProvider extends ServiceProvider
 
     public function boot()
     {
-
-
         $this->mergeConfigFrom(__DIR__.'/config/mobilyws.php', 'mobilyws');
-        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'mobilyws');
-        if(!file_exists(base_path('config').'/mobilyws.php')) {
+        if(!file_exists(config_path('mobilyws.php'))) {
             $this->publishes([__DIR__ . '/config/mobilyws.php' => config_path('mobilyws.php')]);
         }
-        $this->publishes([__DIR__.'/resources/lang/en' => resource_path('lang/en')]);
-        $this->publishes([__DIR__.'/resources/lang/ar' => resource_path('lang/ar')]);
+
+        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'mobilyws');
+        if(!file_exists(resource_path('lang/en/mobilyws.php'))) {
+            $this->publishes([__DIR__ . '/resources/lang/en' => resource_path('lang/en')]);
+            $this->publishes([__DIR__ . '/resources/lang/ar' => resource_path('lang/ar')]);
+        }
 
     }
 
